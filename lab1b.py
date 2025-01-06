7f07c26a2e837d85bea19c2980f4f2c269cb8bab
Olivia Melnic <omelnic@myseneca.ca>
2025-01-06 16:44:48 -0500

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Description: This program will ask for an input for age for Issac'''
+
+# Creates a veriable called "Name" and sets the value to "Issac"
+name = "Isaac";
+
+print('How old are you ' + name + '?');
